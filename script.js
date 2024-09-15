98c3a8565b0ce4415951058ec511e3a9d919d7dc
WebMohit20 <2003mohitkhemani@gmail.com>
2024-09-15 19:33:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let tbody = table.createTBody();
 let tr;
 let td;
 function createTable() {
-   let nOfRow = prompt("Input number of rows" ,"3");
-	let nOfCol = prompt("Input nuber of columns", "3");
+   let nOfRow = prompt("Input number of rows");
+	let nOfCol = prompt("Input number of columns");
 	for(let i = 0; i < nOfRow; i++){
 		tr = tbody.insertRow();
 		for(let j = 0 ; j < nOfCol; j++){
